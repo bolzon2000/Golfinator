8942a87cc8a799e1788b28702dde2fdd363784c4
Dave Bolton <bolzon2000@yahoo.com>
2018-11-07 16:44:40 -0800

    Initial Commit

---
+++ Golfinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Golfinator
+//
+//  Created by dave on 11/7/18.
+//  Copyright © 2018 dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Golfinator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Golfinator
+//
+//  Created by dave on 11/7/18.
+//  Copyright © 2018 dave. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
